ad39f974f05cdb5ca753de88c34b433144328e0b
romuald85 <papeboyedk@gmail.com>
2020-08-09 20:12:53 +0200

    create template show and navigation between pages

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,10 +13,18 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="app_article")
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index(ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findAll();
 
         return $this->render('article/index.html.twig', compact('articles'));
+    }
+
+    /**
+     * @Route("/article/{id<[0-9]+>}", name="app_articles_show")
+     */
+    public function show(Article $article)
+    {
+        return $this->render('article/show.html.twig', compact('article'));
     }
 }
