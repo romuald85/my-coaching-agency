b39e8de17b60cde20ec788eb08df3ba823d98cc1
romuald85 <papeboyedk@gmail.com>
2020-09-22 23:38:36 +0200

    Make form EditProfile and create function editProfile and edit_profile template

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Form\EditProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -15,5 +17,29 @@ class ProfileController extends AbstractController
     public function index()
     {
         return $this->render('profile/index.html.twig');
+    }
+
+    /**
+     * Modifier le profil de l'utilisateur
+     * 
+     * @Route("/profile/edit/{id<[0-9]+>}", name="app_profile_edit")
+     */
+    public function editProfile(Request $request, EntityManagerInterface $em, User $user)
+    {
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            $this->addFlash('success', 'Les informations ont bien été modifiées');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit_profile.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class EditProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastName', TextType::class, [
+                'label' => 'Changer le nom'
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Changer le prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Changer l\'email'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
