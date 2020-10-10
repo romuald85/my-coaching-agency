5306c572042e6e920d0599868896a17ffe542868
romuald85 <papeboyedk@gmail.com>
2020-10-10 22:33:50 +0200

    Refactoring add text to layout pircing

--- src/Controller/PricingController.php
+++ src/Controller/PricingController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PricingController extends AbstractController
 {
     /**
+     * Affiche les produits (programmes dans la page pricing là où il y a les programmes)
      * @Route("/pricing", name="app_pricing")
      */
     public function index(ProductsRepository $productsRepo)
