13cd287107d0fd1785f020a227a65918c8a5583c
romuald85 <papeboyedk@gmail.com>
2020-12-01 15:04:20 +0100

    Extract sender details to container parameters
    	modified:   config/services.yaml

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -58,7 +58,10 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@mycoaching.com', 'My coaching'))
+                    ->from(new Address(
+                        $this->getParameter('app.mail_from_address'),
+                        $this->getParameter('app.mail_from_name')
+                    ))
                     ->to($user->getEmail())
                     ->subject('Veuillez confirmer votre mail')
                     ->htmlTemplate('email/confirmation_email.html.twig')
